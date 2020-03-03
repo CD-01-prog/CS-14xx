1dc76b110a8f2bf3d41b1a6fb6ec499240557b4d
CDelaney0 <46506898+CDelaney0@users.noreply.github.com>
2020-03-03 15:54:05 -0700

    Create Backround.py

---
+++ Illustrate/Backround.py
@@ -0,0 +1,10 @@
+class Backround:
+
+  def __init__(self, color):
+    self.color = color
+    Backround.fillScreen()
+    return
+
+  def fillScreen(self, color)
+    screen.fill(color)
+    return
